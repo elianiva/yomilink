ec13a0a0902ffce68ecfa7033d6f3cdee833ad30
elianiva <git@elianiva.my.id>
2026-04-05 16:45:16 +0700

    fix: missing vite app url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { createAuthClient } from "better-auth/react";
 import { ac, roles } from "./auth-permissions";
 
 export const authClient = createAuthClient({
-	baseURL: import.meta.env.VITE_APP_URL ?? window.location.origin,
+	baseURL: import.meta.env.VITE_APP_URL,
 	plugins: [
 		adminClient({
 			ac,
